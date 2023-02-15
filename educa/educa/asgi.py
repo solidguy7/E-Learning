6f3ede4c2a8e567a3843076c12340f4055363d30
solidguy7 <kirill15grishkov01@gmail.com>
2023-02-15 14:04:13 +0300

    Using Daphne for django channels

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
@@ -9,5 +10,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({'http': django_asgi_app, 'websocket': AuthMiddlewareStack(
-    URLRouter(chat.routing.websocket_urlpatterns))})
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+        )
+    ),
+})
